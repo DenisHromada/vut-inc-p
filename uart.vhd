46ce783d0961af7506b7a457993151d27a23a474
Rhodes <denhro@seznam.cz>
2022-05-08 13:58:18 +0200

    Split CMP logic from counters

--- uart.vhd
+++ uart.vhd
@@ -30,10 +30,15 @@ end UART_RX;
 -------------------------------------------------
 
 architecture behavioral of UART_RX is
-signal cnt_a      : std_logic_vector(2 downto 0) := "000";
-signal cnt_b      : std_logic_vector(3 downto 0) := "0000";
+	--important signals
+signal cnt_a      : std_logic_vector(2 downto 0);
+signal cnt_b      : std_logic_vector(3 downto 0);
 signal fsm_result : std_logic_vector(1 downto 0);
-signal d_out      : std_logic_vector(7 downto 0) := "00000000";
+signal d_out      : std_logic_vector(7 downto 0);
+	--internal signal logic
+signal fsm_rst_sig: boolean;
+signal rst_cnt_a: boolean;
+signal rst_cnt_b: boolean;
 
 begin
 	FSM : entity work.UART_FSM(behavioral)
@@ -46,12 +51,31 @@ begin
 		RESULT => fsm_result
 	);
 
+	p_fsm_rst_sig : process(fsm_result)
+	begin
+		if fsm_result = "00" then
+			fsm_rst_sig <= true;
+		else
+			fsm_rst_sig <= false;
+		end if;
+		--fsm_rst_sig <= true when (fsm_result = "00") else false;
+	end process;
+
+--cnt_a stuff
+
+	p_rst_cmp_a : process(RST, fsm_rst_sig) begin
+		if ((RST = '1') or fsm_rst_sig) then
+			rst_cnt_a <= true;
+		else
+			rst_cnt_a <= false;
+		end if;
+		--rst_cnt_a <= true when ((RST = '1') or fsm_rst_sig) else false;
+	end process;
+
 	p_cnt_a : process(CLK, RST)
 	begin
-		if RST = '1' then
-			cnt_a <= "000";
-		elsif rising_edge(CLK) then
-			if fsm_result = "00" then
+		if rising_edge(CLK) then
+			if rst_cnt_a then
 				cnt_a <= "000";
 			elsif fsm_result(0) = '1' then
 				cnt_a <= cnt_a + 1;
@@ -59,12 +83,21 @@ begin
 		end if;
 	end process;
 
+--cnt_b stuff
+
+	p_rst_cmp_b : process(RST, fsm_rst_sig, cnt_b) begin
+		if ((RST = '1') or fsm_rst_sig or (cnt_b = "1110")) then
+			rst_cnt_b <= true;
+		else
+			rst_cnt_b <= false;
+		end if;
+		--rst_cnt_b <= true when ((RST = '1') or fsm_rst_sig or (cnt_a = "1110")) else false;
+	end process;
+
 	p_cnt_b : process(CLK, RST)
 	begin
-		if RST = '1' then
-			cnt_b <= "0000";
-		elsif rising_edge(CLK) then
-			if fsm_result = "00" or cnt_b = "1110" then
+		if rising_edge(CLK) then
+			if rst_cnt_b then
 				cnt_b <= "0000";
 			elsif fsm_result(1) = '1' then
 				cnt_b <= cnt_b + 1;
